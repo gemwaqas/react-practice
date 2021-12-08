65e3642a4418aa57f8ba6528a4c34f300ddf983d
Muhammad Waqas <gemwaqas@hotmail.com>
2021-12-08 04:18:58 -0800

    first

---
+++ index.js
@@ -0,0 +1,8 @@
+const ibrahim = require("./seconds.js")
+
+console.log("Hello world.", ibrahim)
+console.log("Hello waqas.", ibrahim)
+
+console.log("Hello waq3as.", ibrahim)
+
+console.log("Hello waq3as.", ibrahim)
---
+++ seconds.js
@@ -0,0 +1,10 @@
+const waqas = {
+
+    name: "waqas",
+    age: 42,
+
+
+    developer : true
+}
+
+module.exports= waqas;
